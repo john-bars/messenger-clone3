b416f8e8077d86138db92d75eec4c6a3e560724d
John Bars <john.barsq@gmail.com>
2023-09-19 07:58:22 +0800

    use the react-hook-form built-in props use clsx for dynamic styling. git commit -m

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import clsx from "clsx";
+
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
@@ -19,8 +20,8 @@ const Input: React.FC<InputProps> = ({
   type,
   required,
   disabled,
-  // errors,
-  // register,
+  errors,
+  register,
 }) => {
   return (
     <div>
@@ -35,10 +36,10 @@ const Input: React.FC<InputProps> = ({
         type={type}
         autoComplete={id}
         disabled={disabled}
-        // {...register(id, { required })}
+        {...register(id, { required })}
         className={clsx(
-          `w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ting-inset ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6`,
-          // errors[id] && "focus:ring-rose-500",
+          `form-input w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6`,
+          errors[id] && "focus:ring-rose-500",
           disabled && "opacity-50 cursor-default"
         )}
       />
