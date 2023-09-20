11e10cb7f23af86e1bfcc20c050186f48f519c75
John Bars <john.barsq@gmail.com>
2023-09-20 16:30:30 +0800

    Setup roite that will handle the authentication using NextAuth Use GithubProvider, GoogleProvider, and CredentialsProvider for email signin as provider options. Use bcrypt to handle/encrypt passwords for added security.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,67 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import bcrypt from "bcrypt";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+import prisma from "@/app/libs/prismadb";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  debug: process.env.NODE_ENV === "development",
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
+
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+
+    // Handles Sign in using email and password
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid Credentials");
+        }
+
+        // Return the user's data from the database with the same email from the submitted/credentials email
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid Credentials");
+        }
+
+        // Check if the email's hashedPassword from the database match with the submitted password by the user
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid Credentials");
+        }
+        return user;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as POST, handler as GET };
