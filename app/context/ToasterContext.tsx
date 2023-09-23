c89be2d77c710889eaa729efccceb5ece4761df1
John Bars <john.barsq@gmail.com>
2023-09-23 13:47:57 +0800

    create a TosterContext component that will show notifications for the web app.

---
+++ app/context/ToasterContext.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+// Used to show Notifications
+import { Toaster } from "react-hot-toast";
+
+const ToasterContext = () => {
+  return <Toaster />;
+};
+
+export default ToasterContext;
