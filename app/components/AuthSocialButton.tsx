89b053751425fce961ab31320f57abfc937bd6f0
John Bars <john.barsq@gmail.com>
2023-09-23 14:13:23 +0800

    add react event onClick and type button.

--- app/components/AuthSocialButton.tsx
+++ app/components/AuthSocialButton.tsx
@@ -11,7 +11,11 @@ export const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
   onClick,
 }) => {
   return (
-    <button className="w-full justify-center inline-flex rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0">
+    <button
+      type="button"
+      onClick={onClick}
+      className="w-full justify-center inline-flex rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+    >
       <Icon />
     </button>
   );
