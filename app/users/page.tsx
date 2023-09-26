24e782f4e6b3fc34661350f718ffe1b197a262d0
John Bars <john.barsq@gmail.com>
2023-09-26 14:57:35 +0800

    created the EmptyState component to show chatbox once empty modify the users page layout by adding the EmptyState component.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import EmptyState from "../components/EmptyState";
 
 const Users = () => {
-  return <button onClick={() => signOut()}>Logout</button>;
+  return (
+    <div className="hidden lg:block h-full lg:pl-80">
+      <EmptyState />
+      <button onClick={() => signOut()}>Logout</button>
+    </div>
+  );
 };
 
 export default Users;
