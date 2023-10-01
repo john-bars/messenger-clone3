ad12ce664ed8fb031e3078950ed8c8fe0ca1e844
John Bars <john.barsq@gmail.com>
2023-10-01 17:00:59 +0800

    make UsersLayout an async function given that we needed to fetch data from the database.

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import Sidebar from "../components/sidebar/Sidebar";
 
-const UsersLayout = ({ children }: { children: React.ReactNode }) => {
+const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <Sidebar>
       <div className="h-full">{children}</div>
