508560bf3c73e46012dbf79241289d6b0c2194a1
John Bars <john.barsq@gmail.com>
2023-10-01 17:32:58 +0800

    Use the data fetch from the getCurrentUser() and pass it on the DesktopSidebar as props.

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,6 +1,14 @@
-const Sidebar = ({ children }: { children: React.ReactNode }) => {
+import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+const Sidebar = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
   return (
-    <div>
+    <div className="h-full">
+      <DesktopSidebar currentUser={currentUser!} />
+      <MobileFooter />
       <main className="h-full lg:pl-20">{children}</main>
     </div>
   );
